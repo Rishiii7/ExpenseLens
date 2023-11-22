e7a007a10f8d4f633aafd24b98afa70752b0e693
= <7rishi20ss@gmail.com>
2023-11-21 17:58:53 -0700

    Update ocr.py

--- src/ocr.py
+++ src/ocr.py
@@ -8,7 +8,7 @@ receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt'
 # include libarires regarding that
 
 # Load the image file to analyze
-with open('Sample-Images/Receipt.jpeg', 'rb') as imageFile:
+with open('Sample-Images/Receipt1.jpeg', 'rb') as imageFile:
     imageBytes = imageFile.read()
 
 # Make a POST request with the image bytes in the body of the request
@@ -24,8 +24,5 @@ response = requests.post(url=receiptOcrEndpoint,
 
 
 # Check if the response was successful and print out the result
-if response.status_code == 200:
-    with open('sample-response/response1.json', "w") as f:
-        json.dump(json.loads(response.text), f)
-else:
-    print("Error occurred while making HTTP Request.")
+with open('sample-response/response2.json', "w") as f:
+    json.dump(json.loads(response.text), f)
