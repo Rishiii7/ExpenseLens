00292879a506a613f3e184ace2b5a0bd5f401a8c
= <7rishi20ss@gmail.com>
2023-11-29 22:59:51 -0700

    added class function for modular coding

--- src/ocr/ocr_preprocessing.py
+++ src/ocr/ocr_preprocessing.py
@@ -1,94 +1,65 @@
 import json
-import requests
-import os
-import sys
-
-from ocr_utils import read_file_object
+from src.ocr.ocr_utils import read_file_object
 from dataclasses import dataclass
 
 @dataclass
 class ReceiptInfo:
-        
-    merchant_name :str 
-    zip :int
-    country :str
-    state :str
-    city :str
-    date :str
-    items :dict
-    ocr_text :str
+    merchant_name: str
+    zip_code: int
+    country: str
+    state: str
+    city: str
+    date: str
+    items: list
+    ocr_text: str
 
-
-    def add_info_of_receipt(self, 
-                            receipt :dict):
-        print("Receipt into add function")
-
-        self.merchant_name = receipt['ocr_text'].split('\n').strip()
-        print(self.merchant_name)
-        
-        pass
-    pass
+    @classmethod
+    def from_json(cls, receipt):
+        merchant_name = receipt['ocr_text'].split('\n')[0].strip()
+        return cls(
+            merchant_name=merchant_name,
+            zip_code=receipt.get('zip', 0),
+            country=receipt.get('country', ''),
+            state=receipt.get('state', ''),
+            city=receipt.get('city', ''),
+            date=receipt.get('date', ''),
+            items=receipt.get('items', []),
+            ocr_text=receipt.get('ocr_text', '')
+        )
 
 class ReceiptProcessor:
 
     def __init__(self, file_path):
         self.file_path = file_path
-        #self.Reciept_info_object = ReceiptInfo()
 
     def parse_json(self):
-        raw_ocr_text = read_file_object(self.file_path, "r")
-        # with open(self.file_path, 'r') as file:
-        #     raw_ocr_text = json.load(file)
-        return raw_ocr_text
+        return read_file_object(self.file_path, "r")
 
     def extract_receipt_info(self, parsed_json):
         receipt = parsed_json['receipts'][0]
-        merchant = receipt['ocr_text'].split('\n')[0].strip()
-        location = {
-            "city" : receipt['city'],
-            "state" :receipt['state'],
-            "country" : receipt['country'],
-            "zip" : receipt['zip']
-        }
+        receipt_info = ReceiptInfo.from_json(receipt)
 
-        date = receipt['date']
+        # Additional processing if needed
 
-        product_details = [ (item['description'],item['amount']) for item in receipt['items']]
+        return receipt_info
 
-        total_amount = receipt['total']
-        sub_total_amount = receipt['subtotal']
-        tax_amount = receipt['tax']
-
-        payment_method = receipt['payment_method'] if receipt['payment_method']  else  "card"
-        print(payment_method)
-        print(product_details)
-        print(merchant)
-        #self.Reciept_info_object.add_info_of_receipt(receipt=receipt)
-        
-        return receipt
-
-    def put_info_into_datbases(self):
+    def put_info_into_databases(self):
         """
-        after verficatiobn from user
-        Call a database object
-        that should redirect to database.py
-        and insert the data into it
-        
+        After verification from the user,
+        call a database object that should redirect to database.py
+        and insert the data into it.
         """
         pass
 
-
-    def print_receipt_info(self, receipt):
-        print(f"Your purchase at {receipt['merchant_name']}")
-        for item in receipt['items']:
-            print(f" {item['description']} - ${item['amount']}")
-
+    def print_receipt_info(self, receipt_info):
+        print(f"Your purchase at {receipt_info.merchant_name}")
+        print(f"Zipcode at {receipt_info.zip_code}")
+        for item in receipt_info.items:
+            print(f"{item['description']} - ${item['amount']}")
 
 if __name__ == "__main__":
-
-    file_path = 'sample-response/response2.json' # change it to bucket path
+    file_path = 'sample-response/response3.json'  # Change it to the correct file path
     receipt_processor = ReceiptProcessor(file_path)
     parsed_json = receipt_processor.parse_json()
-    receipt = receipt_processor.extract_receipt_info(parsed_json)
-    receipt_processor.print_receipt_info(receipt)
-
+    receipt_info = receipt_processor.extract_receipt_info(parsed_json)
+    receipt_processor.print_receipt_info(receipt_info)
